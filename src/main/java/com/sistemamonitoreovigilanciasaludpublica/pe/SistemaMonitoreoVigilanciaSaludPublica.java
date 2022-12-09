87760fad671a7d649fcd6aadbbb8601d3a2a6c58
Marco Quiña <marcoq558@gmail.com>
2022-12-08 19:49:36 -0500

    Proyecto Inicial

---
+++ src/main/java/com/sistemamonitoreovigilanciasaludpublica/pe/SistemaMonitoreoVigilanciaSaludPublica.java
@@ -0,0 +1,9 @@
+package com.sistemamonitoreovigilanciasaludpublica.pe;
+
+
+public class SistemaMonitoreoVigilanciaSaludPublica {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
