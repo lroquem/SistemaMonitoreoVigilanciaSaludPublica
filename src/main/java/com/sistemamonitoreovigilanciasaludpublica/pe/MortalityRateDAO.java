1cc6512a4100d3f7597fa612ccf9170ac2e71f89
lroque <lroquem@unsa.edu.pe>
2022-12-12 00:24:02 -0500

    CSV file done. Read and list data

--- src/main/java/com/sistemamonitoreovigilanciasaludpublica/pe/MortalityRateDAO.java
+++ src/main/java/com/sistemamonitoreovigilanciasaludpublica/pe/MortalityRateDAO.java
@@ -65,15 +65,37 @@ import java.util.Scanner;
 public class MortalityRateDAO{
     public List<MortalityRate> all(){
         List<MortalityRate> listMortalityRates = new ArrayList<>();
-        
+        // Cargar el archivo
         File file = new File("D:\\eclipse-workspace\\SistemaMonitoreoVigilanciaSaludPublica\\doc\\data.csv");
         try {
             Scanner inputStream = new Scanner(file);
+            // Leer cada linea
+            // Cada linea tiene 11 campos
+            String data = inputStream.next();
             while(inputStream.hasNext()){
-                String data = inputStream.next();
+                data = inputStream.next();
+                
+                // Separar campos
+                String[] values = data.split(",");
+                for (String value : values) {
+                    System.out.println(value+"******");
+                }
                 
                 MortalityRate mortalityRate = new MortalityRate();
-                System.out.println(data+"*********");
+                mortalityRate.setYear(Integer.parseInt(values[0]));
+                mortalityRate.setDepartment(values[1]);
+                mortalityRate.setGender(values[2]);
+                mortalityRate.setDisease(values[3]);
+                mortalityRate.setNumberDeath(Integer.parseInt(values[4]));
+                mortalityRate.setGrossRate(Float.parseFloat(values[5]));
+                mortalityRate.setAdjusmentRate(Float.parseFloat(values[6]));
+                mortalityRate.setError(Float.parseFloat(values[7]));
+                mortalityRate.setLowerLimit(Float.parseFloat(values[8]));
+                mortalityRate.setUpperLimit(Float.parseFloat(values[9]));
+                mortalityRate.setCluster(Integer.parseInt(values[10]));
+                
+                listMortalityRates.add(mortalityRate);
+                
             }
             inputStream.close();
         } catch (FileNotFoundException e) {
